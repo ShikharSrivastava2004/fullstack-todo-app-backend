215d029c87c0657620a01d34aa1ba877288b54bb
Shikhar Srivastava <143494296+ShikharSrivastava2004@users.noreply.github.com>
2024-05-12 10:43:05 +0530

    first commit

---
+++ routes/ToDoRoute.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const { updateToDo,deleteToDo,getToDo, saveToDo } = require('../controllers/ToDoController');
+
+const router = Router();
+
+router.get('/',getToDo)
+router.post("/save",saveToDo)
+router.post("/update",updateToDo)
+router.delete("/delete",deleteToDo)
+
+
+module.exports = router;
